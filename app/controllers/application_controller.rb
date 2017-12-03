85d60998a9be9ef5738b372b7a48153b0f5f9dc9
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-03 22:11:19 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-end
+  class Item < ActiveRecord::Base
+    belongs_to :category
+    has_many :line_items
+
+    def self.available_items
+      self.where.not(inventory: 0)
+    end
+  end
