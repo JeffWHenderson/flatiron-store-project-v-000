98c4dfd0aa8bf498c8905c206cd75c2c4cf2c56c
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-03 23:02:36 +0000

    automatically backed up by learn

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,7 +2,7 @@ class CartsController < ApplicationController
 
   def show
     @cart = Cart.find_by(id: params[:id])
-    raise current_user.local_methods.inspect
+    raise current_user.inspect
   end
 
   def checkout
